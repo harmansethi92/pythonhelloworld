5fd5a9437718c2c0b916dce18a660e9716835fde
root <root@ip-172-31-30-6.ec2.internal>
2017-10-22 22:02:41 +0000

    first

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-import SimpleHTTPServer
-import SocketServer
+from flask import Flask
+app = Flask(__name__)
 
-PORT = 8500
-
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-
-httpd = SocketServer.TCPServer(("", PORT), Handler)
-
-print "serving at port", PORT
-httpd.serve_forever()
+ 
+@app.route("/")
+def hello():
+    return "Hello World!"
+ 
+if __name__ == "__main__":
+    app.run(host='0.0.0.0' , port=5000)
