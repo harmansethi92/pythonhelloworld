82c5d9c7fe11003f97ec6bb539228c1117d95ed6
root <root@ip-172-31-30-6.ec2.internal>
2017-10-24 22:54:43 +0000

    new

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World! We did it"
+    return "Hello World! Thinking of database"
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
 
