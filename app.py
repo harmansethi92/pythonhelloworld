d4b1fed05feda1f7914b022e8453ca940becc5da
root <root@ip-172-31-78-24.ec2.internal>
2017-10-24 19:52:28 +0000

    first

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
 from flask import Flask
-from redis import Redis
-
 app = Flask(__name__)
-redis = Redis(host='redis', port=6379)
 
-@app.route('/')
+@app.route("/")
 def hello():
-    count = redis.incr('hits')
-    return 'Hello World!,, I have been visited {} times.\n'.format(count)
-
+    return "Hello World!"
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0")
