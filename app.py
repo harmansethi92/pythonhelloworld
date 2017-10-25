f53a5fe0925c3b61b390937da788c1f169085908
root <root@ip-172-31-30-6.ec2.internal>
2017-10-25 17:00:25 +0000

    first

--- app.py
+++ app.py
@@ -1,28 +1,10 @@
 from flask import Flask
-from flaskext.mysql import MySQL
-from flask import Flask, request
-
-mysql = MySQL()
 app = Flask(__name__)
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_DB'] = 'EmpData'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-mysql.init_app(app)
 
+PORT = 5000
 @app.route("/")
 def hello():
-    return "Welcome to Python Flask App!"
-
-@app.route("/Authenticate")
-def Authenticate():
-    username = request.args.get('UserName')
-    password = request.args.get('Password')
-    cursor = mysql.connect().cursor()
-    cursor.execute("SELECT * from User where Username='" + username + "' and Password='" + password + "'")
-    data = cursor.fetchone()
-    if data is None:
-     return "Username or Password is wrong"
-    else:
-     return "Logged in successfully"
+    return "Hello World! yes"
 if __name__ == "__main__":
-    app.run("0.0.0.0")
+    app.run(host="0.0.0.0",port= PORT)
+
