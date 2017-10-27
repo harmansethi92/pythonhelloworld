4135a73cc0b92209231985ee0a9c03cfb60e72a5
root <root@ip-172-31-30-6.ec2.internal>
2017-10-27 22:09:29 +0000

    test

--- app.py
+++ app.py
@@ -4,11 +4,7 @@ app = Flask(__name__)
 PORT = 5000
 @app.route("/")
 def hello():
-<<<<<<< HEAD
-    return "Hello World"
-=======
-    return "Hello World!!"
->>>>>>> d5306fac859717c08a5af7b6a453a75be20f8393
+	return "Hello World!!"
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port= PORT)
 
